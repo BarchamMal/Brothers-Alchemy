07a6b51a9c18aca28aec2598b132cd277455e1d4
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2023-12-20 11:56:50 -0600

    Add NAMESPACE

--- src/main/java/net/barch/brosalch/BrothersAlchemy.java
+++ src/main/java/net/barch/brosalch/BrothersAlchemy.java
@@ -11,6 +11,8 @@ public class BrothersAlchemy implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("brothers-alchemy");
 
+	public static final String NAMESPACE = "brothers-alchemy";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
